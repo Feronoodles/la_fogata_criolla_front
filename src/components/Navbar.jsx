97671453f1c0414a9649949aca19a4082a3cec1b
Fernando <ferourlich@gmail.com>
2023-08-24 01:25:07 -0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <ul className='lista'>
+        <li><a href='#reserva'>Reservación</a></li>
+        <li><a href='#contactos'>Contactos</a></li>
+        <li><a href='#carta'>Carta</a></li>
+        <li><a href='#faq'>FAQ</a></li>
+    </ul>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { FogataApp } from './FogataApp.jsx'
+
+import './styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FogataApp />
+  </React.StrictMode>,
+)
